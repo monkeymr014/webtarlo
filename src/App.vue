f46190756fed66f4c64bc7dad6bc9c9d8417f0c1
monkeymr014 <monkeymr014@gmail.com>
2021-03-23 17:27:52 +0100

    animation crewMobile

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ export default {
 </script>
 
 
-<style lang="scss">
+<style scoped lang="scss">
 
 /* MOBILE */
 h1 {
--- src/components/Mob/MobCrew.vue
+++ src/components/Mob/MobCrew.vue
@@ -1,25 +1,66 @@
 <template>
   <div>
     <h1>Zespół</h1>
-    <div class="parent">
-    Zrób flexbox???
-      <div class="div1">
-        <div class="parent_con" v-on:click="isEnabled = !isEnabled" :class="{ box_active : isEnabled }" ></div>
+    <div class="container">
+      <div class="item">
+        <div class="parent_con" v-on:click="wojtekRaid = !wojtekRaid, rafalRaid = false, bartekRaid = false , sebaRaid = false, michalRaid = false, maciejRaid = false " :class="{ box_active : wojtekRaid }" >
+          <img class="box_media_item" alt="tarło" src="../../assets/crewfoto/wojtek.jpg" />
+        </div>
+        <transition name="fade">
+          <div v-if="wojtekRaid" class="text" >
+            <h3>Wojtek Zając</h3><p>wokalista gitara teksty</p>
+          </div>
+        </transition>
       </div>
-      <div class="div2">
-        <div class="parent_con"></div>
+      <div class="item">
+        <div class="parent_con" v-on:click="rafalRaid = !rafalRaid, wojtekRaid = false, bartekRaid = false , sebaRaid = false, michalRaid = false, maciejRaid = false" :class="{ box_active : rafalRaid }">
+          <img class="box_media_item" alt="tarło" src="../../assets/crewfoto/rafal.jpg" />
+        </div>
+        <transition name="fade">
+          <div v-if="rafalRaid" class="text" >
+            <h3>Rafał Wielgosz</h3><p>gitara teksty</p>
+          </div>
+        </transition>
       </div>
-      <div class="div3">
-        <div class="parent_con"></div>
+      <div class="item">
+        <div class="parent_con" v-on:click="bartekRaid = !bartekRaid, rafalRaid = false, wojtekRaid = false , sebaRaid = false, michalRaid = false, maciejRaid = false" :class="{ box_active : bartekRaid }">
+          <img class="box_media_item" alt="tarło" src="../../assets/crewfoto/bartek.jpg" />
+        </div>
+        <transition name="fade">
+          <div v-if="bartekRaid" class="text" >
+            <h3>Bartek Wielgosz</h3><p>gitara</p>
+          </div>
+        </transition>
       </div>
-      <div class="div4">
-        <div class="parent_con"></div>
+      <div class="item">
+        <div class="parent_con" v-on:click="sebaRaid = !sebaRaid, rafalRaid = false, bartekRaid = false , wojtekRaid = false, michalRaid = false, maciejRaid = false" :class="{ box_active : sebaRaid }">
+          <img class="box_media_item" alt="tarło" src="../../assets/crewfoto/seba.jpg" />
+        </div>
+        <transition name="fade">
+          <div v-if="sebaRaid" class="text" >
+            <h3>Sebastian Pokojowy</h3><p>gitara basowa</p>
+          </div>
+        </transition>
       </div>
-      <div class="div5">
-        <div class="parent_con"></div>
+      <div class="item">
+        <div class="parent_con" v-on:click="michalRaid = !michalRaid, rafalRaid = false, bartekRaid = false , sebaRaid = false, wojtekRaid = false, maciejRaid = false" :class="{ box_active : michalRaid }">
+          <img class="box_media_item" alt="tarło" src="../../assets/crewfoto/michal.jpg" />
+        </div>
+        <transition name="fade">
+          <div v-if="michalRaid" class="text" >
+            <h3>Michał Ruszkiewicz</h3><p>perkusja</p>
+          </div>
+        </transition>
       </div>
-      <div class="div6">
-        <div class="parent_con"></div>
+      <div class="item">
+        <div class="parent_con" v-on:click="maciejRaid = !maciejRaid, rafalRaid = false, bartekRaid = false , sebaRaid = false, michalRaid = false, wojtekRaid = false" :class="{ box_active : maciejRaid }">
+          <img class="box_media_item" alt="tarło" src="../../assets/crewfoto/bartek.jpg" />
+        </div>
+        <transition name="fade">
+          <div v-if="maciejRaid" class="text" >
+            <h3>Maciej Gładysz</h3><p>producent muzyczny</p>
+          </div>
+        </transition>
       </div>
     </div> 
   </div>
@@ -30,12 +71,12 @@ export default {
   name: 'MobCrew',
  data() {
     return {
-      isEnabled: false,
+      wojtekRaid: false,
       rafalRaid:  false,
       bartekRaid: false,
       sebaRaid:   false,
-      michalRaid: false
-
+      michalRaid: false,
+      maciejRaid: false
     };
  }
 }
@@ -53,58 +94,79 @@ h1 {
   left: 50%;
   transform: translate(-50%, -50%);
   }
+.text{
+    position:absolute;
+    background:none;
+     font-size:18px;
+  color:red;
+  top:50%;
+  margin:0;
+  padding:0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+    
 
+}
+
+h3{
+  font-size:24px;
+  margin:0;
+  padding:2px;
+}
+p {
+  font-size:20px;
+  margin:0;
+}
+img {
+  left:0;
+  width: 100%;
+  height: auto;
+  position: relative; 
+  filter:contrast(120%) grayscale(80%) ;
+
+}
 div {
   background:black;
   padding:0;
   color: white;
   position:relative;
- 
+  text-align: center;
  }
 
-.parent {  
- display: grid;
-  grid-template-rows: repeat(5, 1fr);
-  grid-column-gap: 1px;
-  grid-row-gap: 1px;
+.container {
   position:absolute;
   clear: both;
   bottom:0;
   height:80%;
   width:100%;
-  border:solid;
-  border-color:red;
+  display: grid;
+  grid-template-columns: 1fr 50%;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  justify-items: stretch;
+   align-items: stretch;
 }
 
-.div1 { grid-area: 1 / 1 / 2 / 2; }
-.div2 { grid-area: 1 / 2 / 2 / 3; }
-.div3 { grid-area: 2 / 2 / 3 / 3; }
-.div4 { grid-area: 2 / 1 / 3 / 2; }
-.div5 { grid-area: 3 / 1 / 4 / 2; }
-.div6 { grid-area: 3 / 2 / 4 / 3; }
 
-.div1 , .div2 ,.div3,.div4, .div5, .div6 {
-  border:solid;
-  border-color:red;
- 
-}
 
-.parent_con
-{
-  clear: both;
-  bottom:0;
-  height:60%;
-  width:auto;
+.item {
+  position: relative;
+  border:solid 1px;
+  border-color:black;
+  margin:0;
+  padding:0;
+  z-index:1;
+    overflow: hidden;
 }
 
 .box_active {
- background: black;
-  position: fixed;
-  top: 96px;
-  transition: transform 0.4s;
-  width: 100%;
-  height: calc(100vh - 96px) ;
-  z-index: 1;
-  padding-top:6%;
-} 
+filter: blur(5px) brightness(0.6);
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
